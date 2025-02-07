4ea7c5f5c9750265675f4508365be36e85192e35
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2025-02-07 08:41:58 -0500

    initial commit + tools added

---
+++ p2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  p2
+//
+//  Created by Sarah Svitlik on 2/7/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
