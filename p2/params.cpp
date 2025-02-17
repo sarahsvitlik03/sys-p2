4a085f3c958b7426583e10d7c49f644288796609
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2025-02-17 10:51:35 -0500

    updated directory command

--- p2/params.cpp
+++ p2/params.cpp
@@ -8,14 +8,15 @@
 #include "params.hpp"
 #include <getopt.h>
 #include <cstring>
-#include <vector> 
+#include <vector>
+#include <string>
+
 
 using namespace std;
 
 Params::Params(int argc, char* argv[]) {
-    
     struct option longOpts[] = {
-        {"verbose", no_argument, NULL, 'b'},
+        {"verbose", no_argument, NULL, 'v'},
         {"output", required_argument, NULL, 'o'},
         {"dir", required_argument, NULL, 'd'},
         {"help", no_argument, NULL, 0},
@@ -23,15 +24,15 @@ Params::Params(int argc, char* argv[]) {
         {NULL, 0, NULL, 0}
     };
     
-    int code, ch, optx;
+    int ch;
     for (;;) { //Processes each switch
         ch = getopt_long(argc, argv, "ivd:o:h", longOpts, nullptr);
         if (ch == -1) break;
         switch (ch) {
             case 'i': ignore_case = true; break;
             case 'v': verbose = true; break;
-                //case 'd':
-                //case 'o':
+            case 'd': dir = optarg; break;
+            //case 'o': output = optarg; break; This isn't working right now.
             case 'h': usage(); exit(0);
             case '?': //Catch invalid switches
             default: usage(); exit(0);
